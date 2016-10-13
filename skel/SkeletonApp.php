3b3f9da6c5d0d6024a002dc78f140d4c52ecd3b8
Bas Hamar de la Brethonière <b.hamar@inthere.nl>
2016-10-13 23:02:14 +0200

    Add helper functions for file listings and user input

--- skel/SkeletonApp.php
+++ skel/SkeletonApp.php
@@ -1,6 +1,6 @@
 <?php namespace Basje;
 
-class SkeletonApp {
+final class SkeletonApp {
 
   /**
    * @var array
@@ -12,6 +12,7 @@ class SkeletonApp {
    */
   public static function postCreateProjectHook() {
 
+    echo PHP_EOL, PHP_EOL;
     $projectName = self::getProjectName();
     self::registerReplacement( 'projectName', $projectName );
 
@@ -74,6 +75,50 @@ class SkeletonApp {
   }
 
   /**
+   * Returns an array of file names from the current working directory and all
+   * subdirectories. Optionally you can pass a subdirectory to start. The
+   * returned file names are relative to the initial directory.
+   *
+   * @param string $directoryName Starting directory.
+   * @return array Collection of file names.
+   */
+  private static function getFilesInDirectory( $directoryName = '.' ) {
+    // Here we store our result.
+    $files = [];
+    // Strip whitespace, slashes, backslashes and dots from the start of the
+    // string. Then prepend the current working directory to the value. This
+    // is meant as a naive method to prevent people from requesting file
+    // listings from outside this directory, either by accident or on purpose.
+    $safeDirectoryName = ltrim( $directoryName . DIRECTORY_SEPARATOR, "./\\\n\r\t\0\x0B" );
+    $fullDirectoryName = getcwd() . DIRECTORY_SEPARATOR . $safeDirectoryName;
+
+    try {
+      // Create an iterator which recursively traverses all directories and
+      // iterates over all files and directories except dot files.
+      $directoryIterator = new \RecursiveDirectoryIterator(
+          $fullDirectoryName,
+          \FilesystemIterator::NEW_CURRENT_AND_KEY | \FilesystemIterator::SKIP_DOTS
+      );
+      $iterator = new \RecursiveIteratorIterator(
+          $directoryIterator,
+          \RecursiveIteratorIterator::SELF_FIRST
+      );
+    }
+    catch ( \Exception $e ) {
+      // Some error occurred which we will ignore and just pretend that no
+      // files have been found.
+      return $files;
+    }
+    // Get all file names relative to the directory we started in.
+    foreach( $iterator as $file ) {
+      if( $file->isFile() ) {
+        $files[] = substr( $file->getPathname(), strlen( $fullDirectoryName ) );
+      }
+    }
+    return $files;
+  }
+
+  /**
    * Reads files, fills the placeholders with the actual values and writes the
    * new contents to disk.
    *
@@ -104,9 +149,35 @@ class SkeletonApp {
     return rmdir( $directoryName );
   }
 
-  public static function getUserInput( $message ) {
+  /**
+   * Displays a message to the user and waits for an answer.
+   *
+   * @param string $message The message to prompt to the user.
+   * @return string User input from standard in.
+   */
+  private static function getUserInput( $message ) {
     echo $message;
     return rtrim( fgets( STDIN ), PHP_EOL );
+  }
+
+  /**
+   * Asks for the preferred Composer package name to be initialised on project
+   * creation. Mirrors Composers' own pattern for the name for optimal
+   * compatibility.
+   *
+   * @return string Composer package name.
+   */
+  private static function getComposerPackageName() {
+    $packageNamePattern = '[a-z0-9_.-]+/[a-z0-9_.-]+';
+    $message = 'Composer package name (<vendor>/<name>): ';
+    $packageName = self::getUserInput( $message );
+    while( preg_match( $packageNamePattern, $packageName ) !== 1 ) {
+      echo 'The package name a is invalid, it should be lowercase and have a ',
+           'vendor name, a forward slash, and a package name, matching: ',
+           $packageNamePattern, PHP_EOL;
+      $packageName = self::getUserInput( $message );
+    }
+    return $packageName;
   }
 
 }
